218d1ffd6397eaaa5c4f51a15d1e41c8f52f5f1a
Ákos Szabados <szabados.akos@gmail.com>
2017-10-03 18:38:51 +0200

    Added basic logger class to handle logging

---
+++ UdynWindowsService/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UdynWindowsService
+{
+    public enum LogLevel
+    {
+        MUTE = 0,
+        FATAL,
+        ERROR,
+        WARNING,
+        INFO,
+        DEBUG,
+        TOTAL
+    }
+
+    static class Logger
+    {
+        public static LogLevel LogLevel = LogLevel.INFO;
+        public static string LogPath = "noPath";
+
+        public static void Log(string logMessage, LogLevel level = LogLevel.INFO)
+        {
+            if (LogLevel < level)
+                return;
+            using (StreamWriter w = File.AppendText(LogPath))
+            {
+                w.Write("Log level : [" + level.ToString() + "] -- Time : ");
+                w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                    DateTime.Now.ToLongDateString());
+                w.WriteLine(": {0}", logMessage);
+                w.WriteLine("\r\n");
+            }
+        }
+    }
+}
--- UdynWindowsService/UdynWindowsService.cs
+++ UdynWindowsService/UdynWindowsService.cs
@@ -10,42 +10,36 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Configuration;
 using System.IO;
+using Microsoft.Win32;
 
 namespace UdynWindowsService
 {
-    public enum LogLevel
-    {
-        MUTE = 0,
-        FATAL,
-        ERROR,
-        WARNING,
-        INFO,
-        DEBUG,
-        TOTAL
-    }
-
     public partial class UdynWindowsService : ServiceBase
     {
         private HttpClient httpClient;
-        private LogLevel currentLogLevel;
         public UdynWindowsService()
         {
             InitializeComponent();
             httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Clear();
             httpClient.BaseAddress = new Uri("https://ddns.aszabados.eu/");
-            currentLogLevel = LogLevel.DEBUG;
+            Logger.LogLevel = LogLevel.DEBUG;
         }
 
         protected override void OnStart(string[] args)
         {
+            string installDir = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\UdynWindowsService", "ImagePath", "noPath");
+            if (installDir.Equals("noPath"))
+                return;
+            Logger.LogPath = installDir.Replace("UdynWindowsService.exe", "").Trim("\"".ToCharArray()) + "udyn.log";
+
             System.Timers.Timer timer = new System.Timers.Timer
             {
                 Interval = 10000             
             };
             timer.Elapsed += UpdateDyname;
             timer.Start();
-            Log("Udyn DDNS service started!\n  Update interval = " + timer.Interval);
+            Logger.Log("Udyn DDNS service started!\n  Update interval = " + timer.Interval);
         }
 
         private async void UpdateDyname(object sender, System.Timers.ElapsedEventArgs e)
@@ -59,35 +53,21 @@ namespace UdynWindowsService
             string responseData = "";
             try
             {
-                Log("Updating dynamic hostname...", LogLevel.DEBUG);
+                Logger.Log("Updating dynamic hostname...", LogLevel.DEBUG);
                 response = await httpClient.PostAsync("update/", new FormUrlEncodedContent(contentDict)).ConfigureAwait(false);
                 responseData = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                 response.EnsureSuccessStatusCode();
-                Log("Update successful! Response from server: " + responseData, LogLevel.INFO);
+                Logger.Log("Update successful! Response from server: " + responseData, LogLevel.INFO);
             }
             catch (HttpRequestException ex)
             {
-                Log("Update failed: HttpRequest Exception " + ex.Message + "\r\n  Server message: " + responseData, LogLevel.ERROR);
+                Logger.Log("Update failed: HttpRequest Exception " + ex.Message + "\r\n  Server message: " + responseData, LogLevel.ERROR);
             }
         }
 
         protected override void OnStop()
         {
-            Log("Udyn DDNS service stopped");
-        }
-
-        public void Log(string logMessage, LogLevel level = LogLevel.INFO, string path = @"F:\asd\udynlog.txt")
-        {
-            if (currentLogLevel < level)
-                return;
-            using (StreamWriter w = File.AppendText(path))
-            {
-                w.Write("Log level : [" + level.ToString() + "] -- Time : ");
-                w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
-                    DateTime.Now.ToLongDateString());
-                w.WriteLine(": {0}", logMessage);
-                w.WriteLine("\r\n");
-            }
+            Logger.Log("Udyn DDNS service stopped");
         }
     }
 }
